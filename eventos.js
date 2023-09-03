fe32c3b1252e7b239251d51c065919fb8e99b021
elgubii <dgubi11@gmail.com>
2023-09-03 00:17:51 -0300

    Solucion click implementado

--- eventos.js
+++ eventos.js
@@ -1,5 +1,12 @@
-let saludo = document.getElementById("saludoBoton");
+let saludo = document.getElementById("saludoDiv");
 
 saludo.addEventListener('click', () => {
   alert('Hola! Soy el div');
+});
+
+let boton = document.getElementById("saludoBoton");
+
+boton.addEventListener('click', (evento) => {
+  evento.stopPropagation();
+  //alert('Hola!');
 });
